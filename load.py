002f18a952f9dca788b3063fd83e217dafbaeec3
Caroline Brewczak <cbrewczak@Real-G.local>
2026-09-22 20:18:41 +0200

    vehicle_emissions loaded

--- load.py
+++ load.py
@@ -17,10 +17,30 @@ def load_parquet_files():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
 
-        con.execute(f"""
-            -- SQL goes here
+        # Make yellow taxi table
+
+
+        # Make green taxi table
+
+        #make vehicle emissions table
+        con.execute("""
+           DROP TABLE IF EXISTS vehicle_emissions;
+            CREATE TABLE vehicle_emissions AS
+            SELECT * FROM read_csv_auto(
+            'data/vehicle_emissions.csv'
+            );
         """)
-        logger.info("Dropped table if exists")
+
+        n = con.execute(
+            "SELECT COUNT(*) FROM vehicle_emissions"
+            ).fetchone()[0]
+
+        logger.info(f"vehicle_emissions: {n} rows loaded")
+
+
+
+        #Count total rows?
+
 
     except Exception as e:
         print(f"An error occurred: {e}")
