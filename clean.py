7cce42fd6efc6343a16f4624e37a6d83cfac0597
Caroline Brewczak <cbrewczak@Real-G.local>
2026-09-24 15:01:43 +0200

    remove zero miles function working

--- clean.py
+++ clean.py
@@ -8,69 +8,81 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-#---STEP 1: Remove duplicates---
+
+#----DEFINE FUNCTION: Remove duplicates-----
 def remove_duplicates(con, table_name):
+    
     # Count duplicate groups before cleaning
     before_duplicates = con.execute(f"""
-        SELECT COUNT(*)
-        FROM (
-            SELECT *
-            FROM {table_name}
-            GROUP BY ALL
-            HAVING COUNT(*) > 1
-        )
+        SELECT COUNT(*) FROM 
+        (SELECT * FROM {table_name} GROUP BY ALL HAVING COUNT(*) > 1)
     """).fetchone()[0]
 
-    # Remove duplicates
+    #Execution: remove duplicates by creating a new table with distinct rows
     con.execute(f"""
-        CREATE TABLE {table_name}_clean AS
-        SELECT DISTINCT * FROM {table_name};
-        DROP TABLE {table_name};
-        ALTER TABLE {table_name}_clean RENAME TO {table_name};  
-    """)
-    logger.info(f"Removed duplicates from {table_name} table")
+                CREATE TABLE IF NOT EXISTS {table_name}_clean AS
+                SELECT DISTINCT * FROM {table_name};
+            """)
+    logger.info(f"Created deduplicated table {table_name}_clean")
 
-    # Verification query: count duplicate groups after cleaning
+    # Verification query- count after cleaning
     after_duplicates = con.execute(f"""
-        SELECT COUNT(*)
-        FROM (
-            SELECT *
-            FROM {table_name}
-            GROUP BY ALL
-            HAVING COUNT(*) > 1
-        )
+        SELECT COUNT(*) FROM (
+            SELECT * FROM {table_name} GROUP BY ALL HAVING COUNT(*) > 1)
     """).fetchone()[0]
-
     print(f"{table_name} duplicate groups: {before_duplicates} before, {after_duplicates} after")
     logger.info(f"{table_name} duplicate groups: {before_duplicates} before, {after_duplicates} after")
 
 
 
-# STEP 2: Remove trips with zero passengers
+# #----DEFINE FUNCTION: Remove trips with zero passengers-----
 def remove_zero_passenger_trips(con, table_name):
-    # Count trips with zero passengers before cleaning
+    
+    # Count before cleaning
     before_zero_passengers = con.execute(f"""
-        SELECT COUNT(*) 
-        FROM {table_name} 
+        SELECT COUNT(*) FROM {table_name} 
         WHERE passenger_count = 0
     """).fetchone()[0]
 
-    # Remove trips with zero passengers
+    # Execution: Remove trips with zero passengers
     con.execute(f"""
         DELETE FROM {table_name} 
         WHERE passenger_count = 0
     """)
     logger.info(f"Removed trips with zero passengers from {table_name} table")
 
-    # Verification query: count trips with zero passengers after cleaning
+    # Verification query- count after cleaning
     after_zero_passengers = con.execute(f"""
-        SELECT COUNT(*) 
-        FROM {table_name} 
+        SELECT COUNT(*) FROM {table_name} 
         WHERE passenger_count = 0
     """).fetchone()[0]
-
     print(f"{table_name} trips with zero passengers: {before_zero_passengers} before, {after_zero_passengers} after")
     logger.info(f"{table_name} trips with zero passengers: {before_zero_passengers} before, {after_zero_passengers} after")
+
+
+#----DEFINE FUNCTION: Remove trips with zero miles-----
+def remove_zero_mile_trips(con, table_name):
+    
+    # Count trips with zero miles before cleaning
+    before_zero_miles = con.execute(f"""
+        SELECT COUNT(*) FROM {table_name} 
+        WHERE trip_distance = 0
+    """).fetchone()[0]
+
+    # Execution: Remove trips with zero miles
+    con.execute(f"""
+        DELETE FROM {table_name} 
+        WHERE trip_distance = 0
+    """)
+    logger.info(f"Removed trips with zero miles from {table_name} table")
+
+    # Verification query: count trips with zero miles after cleaning
+    after_zero_miles = con.execute(f"""
+        SELECT COUNT(*) FROM {table_name} 
+        WHERE trip_distance = 0
+    """).fetchone()[0]
+    print(f"{table_name} trips with zero miles: {before_zero_miles} before, {after_zero_miles} after")
+    logger.info(f"{table_name} trips with zero miles: {before_zero_miles} before, {after_zero_miles} after")
 
 
 
@@ -82,24 +94,26 @@ def clean_data():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance") 
 
+        for table in ["yellow_trips", "green_trips"]:
         
         # ----- STEP 1: Remove duplicates -----
-        
-        remove_duplicates(con, "yellow_trips")
-        remove_duplicates(con, "green_trips")
-
+            remove_duplicates(con, table)
+    
         # ----- STEP 2: Remove trips with zero passengers -----
-        remove_zero_passenger_trips(con, "yellow_trips")
-        remove_zero_passenger_trips(con, "green_trips")
+            remove_zero_passenger_trips(con, table)
+        
+        #---- STEP 3: Remove trips with zero miles --
+            remove_zero_mile_trips(con, table)
+
+        #---- STEP 4: Remove trips with over 100 miles --- 
+            #remove_over_100_mile_trips(con, table)
+       
    
    
    
    
    
-   
-   
-   
-   
+
    
    
     except Exception as e:
